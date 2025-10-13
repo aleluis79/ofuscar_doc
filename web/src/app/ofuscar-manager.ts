5386e7d2b8cae7c33abf7159552c974df555477a
Alejandro <ale.luis@gmail.com>
2025-10-13 21:00:20 +0000

    add environments

--- web/src/app/ofuscar-manager.ts
+++ web/src/app/ofuscar-manager.ts
@@ -1,22 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfuscarManager {
 
-  URL_API = 'http://localhost:9090/';
-
   http = inject(HttpClient);
   
   ofuscar(texto: string) : Observable<Ofuscar> {
-    return this.http.post<Ofuscar>(this.URL_API + 'ofuscar', { texto });
+    return this.http.post<Ofuscar>(environment.URL_API + '/ofuscar', { texto });
   }
 
   desofuscar(texto: Ofuscar) : Observable<Desofuscar> {
-    return this.http.post<Desofuscar>(this.URL_API + 'desofuscar', texto);
+    return this.http.post<Desofuscar>(environment.URL_API + '/desofuscar', texto);
   }
  
 }
